dab36bc0837085c21ca68f47f092c7639f70eb3d
herbalj <juez247@gmail.com>
2019-03-26 16:22:50 -0400

    brute-force cropping

--- Data/src/preprocess.py
+++ Data/src/preprocess.py
@@ -1,64 +1,71 @@
 import os
 import cv2
+import copy
 import numpy as np
 import argparse
 
-
-def variance_of_laplacian(image):
-    return cv2.Laplacian(image, cv2.CV_64F).var()
-
-
-def parse_frame(filename, form="mp4", frame_gap=1, verbose=True, threshold=300):
-    vidcap = cv2.VideoCapture('.'.join([filename, form]))
-    success,image = vidcap.read()
-    count = 0
-    success = True
-    if os.path.exists("./" + filename) == False:
-        os.mkdir("./"+filename)
-    while success:
-        if count % frame_gap == 0:
-            if verbose:
-                print(count)
-            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            output = image.copy()
-            circles = cv2.HoughCircles(gray, cv2.cv2.HOUGH_GRADIENT, 1.3, 50, 
-                                       param1=5,param2=110,minRadius=58,maxRadius=100)
-            
-            if circles is not None:
-                circles = np.round(circles[0, :]).astype("int")
-                rad = max([r for (x, y, r) in circles])
-                for (x, y, r) in circles:
-                    if r == rad:
-                        mask = np.zeros((output.shape[0],output.shape[1], output.shape[2]),dtype=np.uint8)
-                        cv2.circle(mask,(x, y),r,(255,255,255),-1)
-                        break
-                output &= mask
-                crop = output[y - r - 20:y + r + 20, x - r - 20:x + r + 20, :].copy()
-                gray = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
-                hue = [crop[:, :, i].mean() for i in range(crop.shape[-1])]
-                if gray is not None:
-                    fm = variance_of_laplacian(gray)
-                    if verbose:
-                        print("Laplacian variance: " + str(fm))
-                        print("Average color "+ str(hue))
-                    if fm > threshold and hue[0] > 30 and hue[0] < 50 and hue[1] > 40 and hue[2] > 60:
-                        cv2.imwrite("./" + filename + "/frame%d.jpg" % count, crop)
-            
-            else:
-                if verbose:
-                    print("No pupil is detected in fram {}\n".format(count))
-        success, image = vidcap.read()
-        count += 1
-    return
+def increase_contrast(image, brightness=30, contrast=150):
+    gray = cv2.cvtColor(image.copy(), cv2.COLOR_BGR2GRAY)
+    brightness  = 80 - np.mean(gray)
+    img = np.int16(image)
+    img = img * (contrast/127 + 1) - contrast + brightness
+    img = np.clip(img, 0, 255)
+    img = np.uint8(img)
+    return img
 
 
-##sample_usage: python preprocess.py Diabetic_Retinopathy_Screen False 300 1 mp4
+def center_and_crop(image, size=512):
+    ret = image.copy()
+    x = ret.shape[0]
+    y = ret.shape[1]
+    l = min([x // 2, y // 2])
+    crop = ret[ x // 2 - l : x // 2  + l,  y//2 - l : y//2 + l]
+    crop = cv2.resize(crop, (size, size))
+    return crop
+
+
+def preprocess(image):
+    ret = center_and_crop(image)
+    ret = increase_contrast(ret)
+    return ret
+
+    
 if __name__ == "__main__":
+    # Sample Usage: python preprocess.py input_dir output_dir 0
+    # mode 0: dump npz to output_dir quietly
+    # mode 1: dump .jpegs to output_dir LOUDLY
+    
     parser = argparse.ArgumentParser()
-    parser.add_argument("filename", type=str)
-    parser.add_argument("verbose", type=bool)
-    parser.add_argument("threshold", type=int)
-    parser.add_argument("frame_gap", type=int)
-    parser.add_argument("form", type=str)
+    parser.add_argument('input_dir', type=str)
+    parser.add_argument('output_dir', type=str)
+    parser.add_argument('mode', type=int)
     args = parser.parse_args()
-    parse_frame(args.filename,verbose=args.verbose, frame_gap=args.frame_gap, threshold=args.threshold)
+    
+    try:
+        os.stat(args.output_dir)
+    except:
+        os.mkdir(args.output_dir) 
+    
+    data = []
+
+    for root, dirs, files in os.walk(args.input_dir):
+        for name in files:
+            if ".jpeg" in name:
+                path = root +"/" + name
+                image = cv2.imread(path)
+                res = preprocess(image)
+                
+                if res is None:
+                    print("No fundus is detected in {}".format(path))
+                    continue
+                else:
+                    if args.mode == 0:
+                        data.append(res)
+                    
+                    if args.mode == 1:
+                        print(args.output_dir + "/processed_" + name)
+                        cv2.imwrite(args.output_dir + "/processed_" + name, res)
+    
+    data = np.array(data, dtype=np.uint8)
+    # print(data.shape)
+    np.savez(args.output_dir + "/input.npz")
