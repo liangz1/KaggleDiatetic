d35c053640f4e37d2f8af9858e22a7290679ea5a
Liang Zhang <lena.zhangliang@gmail.com>
2019-03-28 11:34:08 -0400

    remove debug

--- src/pic_to_np.py
+++ src/pic_to_np.py
@@ -5,7 +5,7 @@ import json
 
 
 def pic_to_np():
-    data_dir = '/home/yunhan/kaggle_diabetics/test_tiny_224/'
+    data_dir = '/home/yunhan/kaggle_diabetic/test_tiny_224/'
     names = os.listdir(data_dir)
     print("%d files" % len(names))
 
@@ -21,11 +21,15 @@ def pic_to_np():
         pic = Image.open(data_dir + fname)
         pix = np.array(pic.getdata()).reshape(pic.size[0], pic.size[1], 3)
         Xs.append(pix)
-        print(pix)
-        print(label)
-        return  # debug
+        #print(pix)
+        #print(label)
+        #return  # debug
         if (i + 1) % batch_size == 0:
             np.save('X_%s.npy' % (i + 1), np.stack(Xs))
             np.save('Y_%s.npy' % (i + 1), np.array(labels))
             Xs = []
             labels = []
+
+
+if __name__ == '__main__':
+    pic_to_np()
