b0722f76f635d79a50e2a9a3732247c5d03dc613
Liang Zhang <lena.zhangliang@gmail.com>
2019-03-31 03:39:47 -0400

    multiclass

--- src/train.py
+++ src/train.py
@@ -19,9 +19,11 @@ def parse_arguments():
     parser.add_argument('--lr', dest='lr', type=float,
                         default=5e-4, help="learning rate.")
     parser.add_argument('--input_dim', dest='input_dim', type=int,
-                        default=224, help="Input dimension.")
+                        default=512, help="Input dimension.")
+    parser.add_argument('--output_dim', dest='output_dim', type=int,
+                        default=5, help="Output dimension.")
     parser.add_argument('--model_name', dest='model_name', type=str,
-                        default="inception_v3_50_50", help="Model name.")
+                        default="inception_v3_50_50_5class", help="Model name.")
     parser.add_argument('--data_dir', dest='data_dir', type=str,
                         default="/home/yunhan/batchified", help="Data dir.")
     return parser.parse_args()
@@ -34,11 +36,13 @@ def main(args):
     lr = args.lr
     model_name = args.model_name
     input_dim = args.input_dim
+    output_dim = args.output_dim
     data_dir = args.data_dir
 
     # get model
     model = InceptionDR(model_name=model_name,
                         input_shape=(input_dim, input_dim, 3),
+                        output_dim=output_dim,
                         optimizer='adam',
                         loss='sparse_categorical_crossentropy',
                         lr=lr)
@@ -59,11 +63,11 @@ def main(args):
 
     # train model batchified
     losses = []
-    gen_valid = get_evaluate_batches()
+    gen_valid = get_evaluate_batches(data_dir)
     for epoch in range(num_epochs):
 
         print("Overall Epoch: %d" % (epoch+1))
-        for i, (X, Y) in enumerate(get_train_batches()):      # get training data
+        for i, (X, Y) in enumerate(get_train_batches(data_dir)):      # get training data
             loss = model.train(X, Y, batch_size=32)
             losses.extend(loss)
             if (i+1) % 3 == 0:
